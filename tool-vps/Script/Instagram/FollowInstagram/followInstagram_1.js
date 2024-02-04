b3f50ac56f5816c27e467f7d9fb646d50377b96a
MF Trung.tq <trung.tq@mediafusion.co.jp>
2024-02-05 01:35:15 +0900

    update

--- tool-vps/Script/Instagram/FollowInstagram/followInstagram_1.js
+++ tool-vps/Script/Instagram/FollowInstagram/followInstagram_1.js
@@ -13,49 +13,23 @@ async function followInstagram_1({ browser, profileData, config }) {
         if (!sharePercent) sharePercent = 20;
         let page = await navigation.newTab(browser, 'https://www.instagram.com/');
         await helper.delay(20);
-        // random tìm 3-5 người
-        const count = helper.randomInt(3, 5);
+        // follow tìm 1-2 người
+        const count = helper.randomInt(1, 2);
         for (let i = 1; i <= count; i++) {
-            const text = fakerator.names.name();
             await page.evaluate(() => {
+                function randomInt(x, y) {
+                    return Math.floor(Math.random() * (y - x + 1)) + x;
+                  }
                 try {
-                    document.querySelector('input[type=search]')?.focus();
+                    const arr = document.querySelectorAll('div[role=button]');
+                    const followBtn = Array.from(arr).filter(o => o.textContent.trim().toUpperCase() === 'FOLLOW')
+                    followBtn[randomInt(0, followBtn.length - 1)].click();
                 }
                 catch (e) {
-
+                    return
                 }
             });
-            await keyboard.pressKey(page, text);
             await page.keyboard.press('Enter');
-            await helper.delay(10);
-            // random add 1 bạn
-            await page.evaluate(() => {
-                function randomInt(x, y) {
-                    return Math.floor(Math.random() * (y - x + 1)) + x;
-                }
-                try {
-                    let arr = document.querySelectorAll('div[aria-label="Add friend"]');
-                    return arr[randomInt(0, arr.length - 1)].click();
-                }
-                catch (e) {
-
-                }
-            });
-            await helper.delay(10);
-            await mouse.scrollRandom(page, helper.randomInt(1, 2), helper.randomFloat(250, 400), 0.75);
-            // random follow 1 page
-            await page.evaluate(() => {
-                function randomInt(x, y) {
-                    return Math.floor(Math.random() * (y - x + 1)) + x;
-                }
-                try {
-                    let arr = document.querySelectorAll('div[aria-label="Follow"]');
-                    return arr[randomInt(0, arr.length - 1)].click();
-                }
-                catch (e) {
-
-                }
-            });
             await helper.delay(10);
             await mouse.scrollRandom(page, helper.randomInt(1, 2), helper.randomFloat(250, 400), 0.75);
         }
